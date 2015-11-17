fb36e50101a105b432972622d67158ec23b82538
Stuart Ramsay <tartansinatra2@yahoo.co.uk>
2015-11-17 22:56:26 +0000

    latest Ruby Functions Practice file

--- ruby_functions_practice 2/ruby_functions_practice.rb
+++ ruby_functions_practice 2/ruby_functions_practice.rb
@@ -1,19 +1,53 @@
-#def return_10(test_result)
-#  10  
-#end
+# LATEST FILE - 21.13PM
+# def return_10
+#  10
+# end
 
 def add(number_1, number_2)
-  result number_1 + number_2
+  return number_1 + number_2
 end
 
 def subtract(number_1, number_2)
-  result number_1 - number_2
+  return number_1 - number_2
 end
 
 def multiply(number_1, number_2)
-  result number_1 * number_2
+  return number_1 * number_2
 end
 
 def divide(number_1, number_2)
-  result number_1 / number_2
+  return number_1 / number_2
 end
+
+def length_of_string(test_string)
+  test_string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i  
+end
+
+def number_to_full_month_name(month)
+  return case month
+  when 1
+    print_message = "January"
+  when 3
+    print_message = "March"
+  when 9
+    print_message = "September"
+end
+end
+
+
+#####USING THE SAME FUNCTION AS ABOVE, JUST EXTRACTING THE FIRST 3 CHARS###
+def number_to_short_month_name(number)
+  number_to_full_month_name(number)[0..2]
+end
+
+
+
+
--- ruby_functions_practice 2/ruby_functions_practice_runner.rb
+++ ruby_functions_practice 2/ruby_functions_practice_runner.rb
@@ -22,35 +22,35 @@ divide_result = divide( number_1, number_2 )
 puts "#{number_1} / #{number_2} = #{divide_result}"
 
 ##### String Play
-# test_string = "A string of length 21"
-# length_of_string = length_of_string(test_string)
-# puts "#{test_string} is length #{length_of_string}"
+test_string = "A string of length 21"
+length_of_string = length_of_string(test_string)
+puts "#{test_string} is length #{length_of_string}"
 
-# string_1 = "Mary had a little lamb, "
-# string_2 = "it's fleece was white as snow"
-# joined_string = join_string(string_1, string_2)
-# puts "Nursery rhyme: #{joined_string}"
+string_1 = "Mary had a little lamb, "
+string_2 = "it's fleece was white as snow"
+joined_string = join_string(string_1, string_2)
+puts "Nursery rhyme: #{joined_string}"
 
-#####Type conversion
-# string_1 = '1'
-# string_2 = '2'
-# add_result = add_string_as_number(string_1, string_2)
-# puts "#{string_1} + #{string_2} = 3, (not 12!)"
+####Type conversion
+string_1 = '1'
+string_2 = '2'
+add_result = add_string_as_number(string_1, string_2)
+puts "#{string_1} + #{string_2} = 3, (not 12!)"
 
-#####Conditional logic
-# first_month_string = number_to_full_month_name(1)
-# third_month_string = number_to_full_month_name(3)
-# ninth_month_string = number_to_full_month_name(9)
-# puts "The first month is #{first_month_string}"
-# puts "The third month is #{third_month_string}"
-# puts "The ninth month is #{ninth_month_string}"
+####Conditional logic
+first_month_string = number_to_full_month_name(1)
+third_month_string = number_to_full_month_name(3)
+ninth_month_string = number_to_full_month_name(9)
+puts "The first month is #{first_month_string}"
+puts "The third month is #{third_month_string}"
+puts "The ninth month is #{ninth_month_string}"
 
-# first_month_string = number_to_short_month_name(1)
-# third_month_string = number_to_short_month_name(3)
-# ninth_month_string = number_to_short_month_name(9)
-# puts "The first month in 3 letters is #{first_month_string}"
-# puts "The third month in 3 letters is #{third_month_string}"
-# puts "The ninth month in 3 letters is #{ninth_month_string}"
+first_month_string = number_to_short_month_name(1)
+third_month_string = number_to_short_month_name(3)
+ninth_month_string = number_to_short_month_name(9)
+puts "The first month in 3 letters is #{first_month_string}"
+puts "The third month in 3 letters is #{third_month_string}"
+puts "The ninth month in 3 letters is #{ninth_month_string}"
 
 
 ##### Further Challenges (not required),  try to write the expectation first if you can.
